1f658902a7f07e7e13a368b072b6373613dd1c0e
ritikchelani <mysd.ghg@gmail.com>
2022-09-07 20:33:41 +0530

    commit

---
+++ src/data/index.js
@@ -0,0 +1,84 @@
+const employeesData = [
+    {
+      id: 1,
+      firstName: 'Harshit',
+      lastName: 'Kapoor',
+      city: 'Delhi',
+      salary: '95000',
+      date: '2019-04-11'
+    },
+    {
+      id: 2,
+      firstName: 'Kareena',
+      lastName: 'Kapoor',
+      city: 'Jaipur',
+      salary: '80000',
+      date: '2019-04-17'
+    },
+    {
+      id: 3,
+      firstName: 'Aman',
+      lastName: 'Singh',
+      city: 'Gurgaon',
+      salary: '79000',
+      date: '2019-05-01'
+    },
+    {
+      id: 4,
+      firstName: 'Tony',
+      lastName: 'Stark',
+      city: 'Khandala',
+      salary: '56000',
+      date: '2019-05-03'
+    },
+    {
+      id: 5,
+      firstName: 'Peter',
+      lastName: 'Parker',
+      city: 'Lucknow',
+      salary: '65000',
+      date: '2019-06-13'
+    },
+    {
+      id: 6,
+      firstName: 'Ritik',
+      lastName: 'Chelani',
+      city: 'Alwar',
+      salary: '120000',
+      date: '2019-07-30'
+    },
+    {
+      id: 7,
+      firstName: 'George',
+      lastName: 'Khan',
+      city: 'Chennai',
+      salary: '90000',
+      date: '2019-08-15'
+    },
+    {
+      id: 8,
+      firstName: 'Pablo',
+      lastName: 'Escobbar',
+      city: 'Shimla',
+      salary: '60000',
+      date: '2019-10-10'
+    },
+    {
+      id: 9,
+      firstName: 'Rahber',
+      lastName: 'Hussain',
+      city: 'Raipur',
+      salary: '71000',
+      date: '2019-10-15'
+    },
+    {
+      id: 10,
+      firstName: 'ramaswami',
+      lastName: 'iyyer',
+      city: 'Tiruvanthpuram',
+      salary: '110000',
+      date: '2020-01-15'
+    }
+  ];
+  
+  export { employeesData };
